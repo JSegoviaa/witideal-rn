c8526ca3dd62085096d1a362a345075b40639d15
JSegovia <acosta.segoviaa@gmamil.com>
2021-10-13 12:11:50 -0500

    Botones a mis inmuebles

--- src/components/MyPropertiesScreen/MyPropertiesListItem.tsx
+++ src/components/MyPropertiesScreen/MyPropertiesListItem.tsx
@@ -23,6 +23,16 @@ const MyPropertiesListItem = ({ inmueble }: Props) => {
           <Text style={styles.textCenter}>{inmueble.title}</Text>
 
           <Text style={styles.subtitle}>{inmueble.desc}</Text>
+
+          <View style={{ flexDirection: 'row',padding:10 }}>
+            <TouchableOpacity style={styles.btnActivate} >
+              <Text style={{ color: 'white' }}>Activar inmueble</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.btnDest}>
+              <Text style={{ color: '#3F19F9' }}>Destacar inmueble</Text>
+            </TouchableOpacity>
+          </View>
         </TouchableOpacity>
       </View>
     </>
@@ -30,23 +40,41 @@ const MyPropertiesListItem = ({ inmueble }: Props) => {
 };
 
 const styles = StyleSheet.create({
+  btnActivate: {
+    alignItems: 'center',
+    backgroundColor: '#3F19F9',
+    borderRadius: 50,
+    flex: 1,
+    padding: 7,
+    marginRight:10
+  },
+  btnDest: {
+    padding: 7,
+    alignItems: 'center',
+    flex: 1,
+    borderColor: '#63C5FA',
+    borderRadius: 50,
+    borderWidth: 1,
+    marginLeft:10
+
+  },
   card: {
     elevation: 1,
     borderRadius: 2,
     marginBottom: 10,
     marginTop: 10,
   },
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 15,
+    color: '#000',
+    padding: 5,
+  },
   textCenter: {
     textAlign: 'center',
     fontSize: 18,
     color: '#1e0e6f',
     fontWeight: '700',
-    padding: 5,
-  },
-  subtitle: {
-    textAlign: 'center',
-    fontSize: 15,
-    color: '#000',
     padding: 5,
   },
 });
