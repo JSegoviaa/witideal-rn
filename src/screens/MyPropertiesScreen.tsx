d437c2bbd48314bd5e779d45c3f29f6b8db2e8ee
JSegovia <acosta.segoviaa@gmamil.com>
2021-10-27 16:04:17 -0500

    Se muestran mis propiedades

--- src/screens/MyPropertiesScreen.tsx
+++ src/screens/MyPropertiesScreen.tsx
@@ -13,45 +13,6 @@ import { AuthContext } from '../context/auth/AuthContext';
 import { useMyProperties } from '../hooks/useMyProperties';
 import { appStyles } from '../theme/appTheme';
 
-const inmuebles = [
-  {
-    id: 1,
-    title: 'Inmueble 1',
-    desc: 'Descripción 1',
-    img: 'https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/witideal%2FJ7idPjx6jVMlYtzQVF3RbfiWre72%2FAdHaIDPSe3a2BGl5EdzB%2Fthumb%401100_488011_4.jpg?alt=media',
-  },
-  {
-    id: 2,
-    title: 'Inmueble 2',
-    desc: 'Descripción 2',
-    img: 'https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/witideal%2FJ7idPjx6jVMlYtzQVF3RbfiWre72%2FxpBJNkkSSUzm3gRzjyxS%2Fthumb%401100_399429_descarga.jpg?alt=media',
-  },
-  {
-    id: 3,
-    title: 'Inmueble 3',
-    desc: 'Descripción 3',
-    img: 'https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/witideal%2FJ7idPjx6jVMlYtzQVF3RbfiWre72%2FSlmQwNroraXIloP6EzKn%2Fthumb%401100_301215_3.jpg?alt=media',
-  },
-  {
-    id: 4,
-    title: 'Inmueble 4',
-    desc: 'Descripción 4',
-    img: 'https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/witideal%2FJ7idPjx6jVMlYtzQVF3RbfiWre72%2Fx9LUhwIgP8EM3aBR626C%2Fthumb%401100_275716_125400261.jpg?alt=media',
-  },
-  {
-    id: 5,
-    title: 'Inmueble 5',
-    desc: 'Descripción 5',
-    img: 'https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/witideal%2FJ7idPjx6jVMlYtzQVF3RbfiWre72%2FbToynbJrydhGQXHaBwge%2Fthumb%401100_330959__FI.jpg?alt=media',
-  },
-  {
-    id: 6,
-    title: 'Inmueble 6',
-    desc: 'Descripción 6',
-    img: 'https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/witideal%2FJ7idPjx6jVMlYtzQVF3RbfiWre72%2FAdHaIDPSe3a2BGl5EdzB%2Fthumb%401100_488011_4.jpg?alt=media',
-  },
-];
-
 const NoProperties = () => {
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
@@ -73,7 +34,7 @@ const NoProperties = () => {
 
 const MyProperties = () => {
   const { user } = useContext(AuthContext);
-  const { loading,properties } = useMyProperties(user?.uid!);
+  const { loading, properties } = useMyProperties(user?.uid!);
 
   return (
     <SafeAreaView style={{ backgroundColor: '#fff' }}>
