2c7c452d506272de440316f23ab4eaa91016d8f8
JSegovia <acosta.segoviaa@gmamil.com>
2021-11-01 13:17:15 -0500

    Se crea screen de aviso de privacidad

--- src/screens/NoPScreen.tsx
+++ src/screens/NoPScreen.tsx
@@ -1,14 +1,257 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { appStyles } from '../theme/appTheme';
 
 const NoPScreen = () => {
   return (
-    <ScrollView>
+    <ScrollView style={appStyles.container}>
       <View>
-        <Text>Aviso de privacidad</Text>
+        <Text style={styles.text}>
+          De conformidad con lo dispuesto en (i) la Ley Federal de Protección de
+          Datos Personales en Posesión de los Particulares (la “LFPD”); (ii) el
+          Reglamento de la LFPD; y (iii) los Lineamientos del Aviso de
+          Privacidad publicados en el Diario Oficial de la Federación,
+          Desarrolladora de Tecnologías ARD, S.A. de C.V. (“Witideal”), como
+          Responsable del tratamiento de los Datos Personales que recaba, pone a
+          disposición del público en general, así como de sus clientes y
+          usuarios (cada uno referido como “Titular”) el presente Aviso de
+          Privacidad (el “Aviso”).
+        </Text>
+        <Text style={styles.text}>
+          Witideal requiere someter a tratamiento determinados datos personales
+          con el objeto de estar en condiciones de prestar sus servicios.
+          Witideal puede obtener estos datos por cualquiera de las siguientes
+          formas: (i) personalmente del Titular, a través de formatos, llamadas,
+          mensajes y otras comunicaciones presenciales o electrónicas; (ii) a
+          través de su sitio web (https://witideal.com/) y/o a través de
+          cualquier vínculo, micrositio o acceso directamente relacionado con el
+          mismo o a través de las redes sociales administradas por Witideal (el
+          “Sitio”); y/o (iii) a través de los aplicativos móviles que utiliza
+          para prestar sus servicios (la “Aplicación[ILT1] ”).
+        </Text>
+        <Text style={styles.text}>
+          Witideal se reserva el derecho de modificar en cualquier momento el
+          contenido del presente Aviso. Cualquier cambio en el Aviso le será
+          informado al Titular a través de una notificación general en el Sitio
+          o en la Aplicación. La modificación correspondiente entrará en vigor a
+          partir del día siguiente de su publicación.
+        </Text>
+      </View>
+
+      <View>
+        <Text style={styles.title}>Identidad y Domicilio de Witideal</Text>
+        <Text style={styles.text}>
+          Witideal es una sociedad mercantil debidamente constituida de
+          conformidad con la legislación mexicana, tal y como consta en la
+          escritura pública No. 91,244 de fecha 12 de junio de 2019, pasada ente
+          la fe del Notario No. 242 de la Ciudad de México, Lic. Roberto Garzón
+          Jiménez.
+        </Text>
+      </View>
+
+      <View>
+        <Text style={styles.title}>Datos Personales sujetos a tratamiento</Text>
+        <Text style={styles.text}>
+          Los datos del Titular que Witideal someterá a tratamiento son los
+          siguientes:
+        </Text>
+        <Text style={styles.text}>
+          De los Promotores: nombre, correo electrónico, registro federal de
+          contribuyentes (y datos de facturación en general), datos para la
+          realización de cobros inherentes al servicio y la información general
+          de aquellos inmuebles que desea promocionar.
+        </Text>
+        <Text style={styles.text}>
+          De los Buscadores: nombre, teléfono, correo electrónico y ciertos
+          datos relacionados con sus intereses inmobiliarios.
+        </Text>
+        <Text style={styles.text}>
+          El Titular reconoce que los datos personales antes mencionados podrán
+          almacenarse utilizando almacenamiento en la nube. En su caso, Witideal
+          se asegurará de que el proveedor correspondiente cumpla con las
+          disposiciones del Reglamento de la LFPD.
+        </Text>
+        <Text style={styles.text}>
+          Witideal en ningún momento recabará del Titular datos que puedan ser
+          considerados como sensibles de acuerdo con la LFPD.
+        </Text>
+      </View>
+
+      <View>
+        <Text style={styles.title}>Finalidades del tratamiento</Text>
+        <Text style={styles.text}>
+          Los datos personales tratados por Witideal serán utilizados para
+          llevar a cabo las siguientes finalidades:
+        </Text>
+        <Text style={styles.text}>I. Finalidades primarias</Text>
+        <Text style={styles.text}>
+          {'  '}a. Identificar al Titular, corroborar su identidad (a través de
+          mensajes SMS o por cualquier otro medio que facilite su
+          identificación) y crear un perfil o credencial de usuario a través del
+          cual pueda utilizar los servicios ofrecidos por Witideal;
+        </Text>
+        <Text style={styles.text}>
+          {'  '}b. Promover los inmuebles de los Promotores con los Buscadores
+          y, en general, prestar los servicios de promoción e intermediación
+          ofrecidos por Witideal;
+        </Text>
+        <Text style={styles.text}>
+          {'  '}c. Intercambiar la información de contacto de los Promotores y
+          Buscadores (nombre, teléfono y/o correo electrónico, exclusivamente),
+          con el fin exclusivo de crear un posible canal de comunicación entre
+          ellos y de facilitar una posible contratación inmobiliaria.
+        </Text>
+        <Text style={styles.text}>II. Finalidades secundarias</Text>
+        <Text style={styles.text}>
+          {'  '}a. Ofrecer actualizaciones, noticias y/o publicidad que pudiera
+          ser de interés para el Titular.
+        </Text>
+        <Text style={styles.text}>
+          El Titular podrá manifestar su negativa al tratamiento de sus datos
+          con las finalidades secundarias, a través del envío de un correo
+          electrónico en dicho sentido a legal@witideal.com. El correo
+          electrónico referido deberá incluir, en todo caso, la información que
+          permita identificar al Titular, la información sobre la cual niega su
+          tratamiento y cualquier otra información que resulte necesaria para
+          tramitar la negativa correspondiente.
+        </Text>
+      </View>
+
+      <View>
+        <Text style={styles.title}>Consentimiento del Titular</Text>
+        <Text style={styles.text}>El Titular manifiesta que:</Text>
+        <Text style={styles.text}>
+          a. Ha leído, entendido y aceptado los términos y condiciones
+          establecidos en el presente Aviso, por lo que otorga su consentimiento
+          respecto del tratamiento de sus datos personales en los términos que
+          en él se establecen;
+        </Text>
+        <Text style={styles.text}>
+          b. Consiente el tratamiento de sus datos personales para las
+          finalidades primarias y secundarias antes establecidas,
+          específicamente en relación con la transferencia de sus datos en los
+          términos del inciso I, fracción c de la sección anterior;
+        </Text>
+        <Text style={styles.text}>
+          c. El uso de o la permanencia en el Sitio, así como la descarga o
+          utilización de la Aplicación constituyen confirmaciones de su
+          consentimiento a lo dispuestos en el presente Aviso. El Titular deberá
+          abstenerse de utilizar el Sitio o la Aplicación en caso de que no
+          consienta con las disposiciones establecidas en el presente Aviso.
+        </Text>
+        <Text style={styles.text}>
+          Witideal asume que la información proporcionada por el Titular le
+          pertenece a este último. En caso de que no sea así, el Titular deberá
+          informar dicha circunstancia inmediatamente a Witideal, a través del
+          envío de un correo electrónico a legal@witideal.com, el cual deberá
+          contener su información de contacto y cualquier otra información que
+          permita dar respuesta a dicho informe, absteniéndose de proporcionar
+          información adicional de la que no sea titular o de la que no tenga
+          autorización para transferir.
+        </Text>
+        <Text style={styles.text}>
+          El Titular podrá revocar el consentimiento antes referido en todo
+          momento. Para revocar el consentimiento proporcionado, el Titular
+          deberá comunicar dicha circunstancia a Witideal por medio de un correo
+          electrónico enviado a legal@witideal.com, indicando las causas que lo
+          motivan a comunicar la revocación, así como la información que permita
+          a Witideal responder y dar seguimiento a dicha revocación.
+        </Text>
+        <Text style={styles.text}>
+          En caso de que el Titular desee limitar el uso o la divulgación de
+          alguno de sus datos personales, podrá en cualquier momento enviar la
+          manifestación de dicha limitación a través de un correo electrónico
+          dirigido a legal@witideal.com, en el cual incluirá los datos
+          personales cuyo tratamiento desea limitar, los motivos por los cuales
+          desea limitarlos, así como la información que permita a Witideal dar
+          seguimiento a dicha solicitud. En caso de ser procedente la solicitud
+          del Titular, Witideal registrará al Titular en la lista de exclusión
+          que tendrá elaborada para tal efecto.
+        </Text>
+      </View>
+
+      <View>
+        <Text style={styles.title}>
+          Derechos de Acceso, Rectificación, Cancelación y Oposición (“ARCO”)
+        </Text>
+        <Text style={styles.text}>
+          Los Titulares tienen derecho a: (i) conocer qué datos personales son
+          tratados por Witideal y las finalidades de su tratamiento (derecho a
+          acceso); (ii) solicitar la corrección de sus datos personales en caso
+          de que estén desactualizados, sean inexactos o estén incompletos
+          (derecho de rectificación); (iii) que sus datos personales sean
+          eliminados de los registros o bases de datos de Witideal cuando
+          consideren que no están siendo utilizados adecuadamente (derecho de
+          cancelación); y (iv) oponerse al uso de sus datos personales para
+          fines específicos (derecho a oposición) (en su conjunto, los derechos
+          “ARCO”)
+        </Text>
+        <Text style={styles.text}>
+          Los derechos ARCO pueden ser ejercidos en cualquier momento a través
+          del envío de un correo electrónico dirigido a legal@witideal.com. La
+          solicitud de cualquiera de los derechos ARCO deberá ir acompañada de
+          lo siguiente:
+        </Text>
+        <Text style={styles.text}>
+          I. El nombre del titular y/o su domicilio o correo electrónico, a
+          efecto de que Witideal pueda comunicarle la respuesta a su solicitud;
+        </Text>
+        <Text style={styles.text}>
+          II. Los documentos que acrediten la identidad o, en su caso, la
+          representación legal del titular;
+        </Text>
+        <Text style={styles.text}>
+          III. La descripción clara y precisa de los datos personales respecto
+          de los que se busca ejercer alguno de los derechos ARCO;
+        </Text>
+        <Text style={styles.text}>
+          IV. Los motivos que fundamentan o justifican el ejercicio del derecho
+          ARCO correspondiente;
+        </Text>
+        <Text style={styles.text}>
+          V. El formato o el medio en el que desea que Witideal dé respuesta a
+          su solicitud, cuando resulte aplicable;
+        </Text>
+        <Text style={styles.text}>
+          VI. Cualquier otro elemento o documento que facilite el seguimiento a
+          la solicitud del Titular.
+        </Text>
+      </View>
+
+      <View>
+        <Text style={styles.title}>
+          Uso de cookies, web beacons o tecnologías similares o análogas
+        </Text>
+        <Text style={styles.text}>
+          Witideal podrá utilizar cookies, web beacons y u otras tecnologías
+          para monitorear su comportamiento como usuario de internet, con la
+          finalidad de brindarle un mejor servicio y experiencia de usuario al
+          navegar en el Sitio, así como ofrecerle nuevos productos basados en su
+          preferencia. Los datos personales que Witideal obtiene de estas
+          tecnologías de rastreo son particularmente los siguientes: horario de
+          navegación, tiempo de navegación, secciones consultadas y páginas de
+          internet accedidas previamente, dirección IP de origen, navegador
+          utilizado, sistema operativo, siendo posible monitorear su
+          comportamiento como usuario de los servicios de internet.
+        </Text>
       </View>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  aqui: { color: '#3F19F9', fontWeight: '700' },
+  text: {
+    color: '#000',
+    fontSize: 15,
+    paddingVertical: 10,
+    textAlign: 'justify',
+  },
+  title: {
+    color: '#000',
+    fontSize: 18,
+    fontWeight: '700',
+  },
+});
 
 export default NoPScreen;
