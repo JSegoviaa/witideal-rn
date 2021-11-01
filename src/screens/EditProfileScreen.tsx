95c18f38f3c3f90356b0425d699577f7b62b96c7
JSegovia <acosta.segoviaa@gmamil.com>
2021-11-01 17:35:02 -0500

    Se añaden validaciones a editar perfil

--- src/screens/EditProfileScreen.tsx
+++ src/screens/EditProfileScreen.tsx
@@ -2,39 +2,59 @@ import React, { useContext } from 'react';
 import { Alert, StyleSheet, Text, TextInput, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import firestore from '@react-native-firebase/firestore';
+import { StackScreenProps } from '@react-navigation/stack';
 import { AuthContext } from '../context/auth/AuthContext';
 import { useForm } from '../hooks/useForm';
 import { useUserInfo } from '../hooks/useUserInfo';
 import { appStyles } from '../theme/appTheme';
+import { RootProfileStackNavigation } from '../navigation/ProfileStackNavigation';
 
-const EditProfileScreen = () => {
+interface Props
+  extends StackScreenProps<RootProfileStackNavigation, 'EditProfileScreen'> {}
+
+const EditProfileScreen = ({ navigation }: Props) => {
   const { user } = useContext(AuthContext);
   const { userInfo } = useUserInfo(user?.uid!);
 
   const { form, onChange } = useForm({
     name: '',
     lastname: '',
-    mail: '',
     phone: '',
   });
 
-  const { name, lastname, mail, phone } = form;
-  const data = { name, lastname, mail, phone };
+  const { name, lastname, phone } = form;
+  const data = { name, lastname, phone };
 
   const handleEditProfile = async () => {
-    await firestore()
-      .collection('production')
-      .doc('Users')
-      .collection(user?.uid!)
-      .doc('generalInfo')
-      .update(data);
+    if (!name.trim()) {
+      Alert.alert('', 'El nombre es obligatorio', [{ text: 'Regresar' }]);
+    }
+    if (!lastname.trim()) {
+      Alert.alert('', 'El apellido es obligatorio', [{ text: 'Regresar' }]);
+    }
+    if (!phone.trim()) {
+      Alert.alert('', 'El número teléfono es obligatorio', [
+        { text: 'Regresar' },
+      ]);
+    }
+
+    if (name.length > 0 && lastname.length > 0 && phone.length > 0) {
+      await firestore()
+        .collection('production')
+        .doc('Users')
+        .collection(user?.uid!)
+        .doc('generalInfo')
+        .update(data);
+
+      navigation.push('ProfileScreen');
+    }
   };
 
   return (
     <View>
       {userInfo && (
         <TextInput
-          value={userInfo.name}
+          placeholder="Escriba su nombre"
           placeholderTextColor="#000"
           style={styles.input}
           onChangeText={value => onChange(value, 'name')}
@@ -43,24 +63,16 @@ const EditProfileScreen = () => {
 
       {userInfo && (
         <TextInput
-          placeholder={`${userInfo.lastname}`}
+          placeholder={`Escriba su apellido`}
           placeholderTextColor="#000"
           style={styles.input}
           onChangeText={value => onChange(value, 'lastname')}
         />
       )}
-      {userInfo && (
-        <TextInput
-          placeholder={`${userInfo.mail}`}
-          placeholderTextColor="#000"
-          style={styles.input}
-          onChangeText={value => onChange(value, 'mail')}
-        />
-      )}
 
       {userInfo && (
         <TextInput
-          placeholder={`${userInfo.phone}`}
+          placeholder={`Escriba su número de teléfono`}
           keyboardType="numeric"
           placeholderTextColor="#000"
           style={styles.input}
