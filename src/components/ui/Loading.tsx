0b42f2f45cce109ef8ebdebc510419c41aef0d12
JSegovia <acosta.segoviaa@gmamil.com>
2021-11-03 18:16:27 -0500

    Se añaden valores por defecto al botón de cargando

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { ActivityIndicator, ActivityIndicatorProps, View } from 'react-native';
 
-const Loading = ({ size, color }: ActivityIndicatorProps) => {
+const Loading = ({
+  size = 'large',
+  color = '#3F19F9',
+}: ActivityIndicatorProps) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
       <ActivityIndicator color={color} size={size} />
