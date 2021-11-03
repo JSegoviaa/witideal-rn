4007cf3af56ba03b1c692c1d7d54999e33cbb222
JSegovia <acosta.segoviaa@gmamil.com>
2021-11-03 12:26:18 -0500

    props

--- src/components/MyPropertiesScreen/MyPropertiesList.tsx
+++ src/components/MyPropertiesScreen/MyPropertiesList.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import MyPropertiesListItem from './MyPropertiesListItem';
 
-interface Props {
-  id: Number;
-  title: String;
-  img: String;
-}
-
-const MyPropertiesList = ({ inmueble }: Props) => {
-  return <MyPropertiesListItem inmueble={inmueble}  />;
+const MyPropertiesList = ({ inmueble }: any) => {
+  return <MyPropertiesListItem inmueble={inmueble} />;
 };
 
 export default MyPropertiesList;
