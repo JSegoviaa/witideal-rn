3ec7f54f0c634aff8b52bb4eb2a401bf635ce755
JSegovia <acosta.segoviaa@gmamil.com>
2021-11-08 12:10:26 -0500

    Se crea ruta desde la posición del usuario al inmueble y se agrega marcados

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
-import MapView, { Circle, Marker } from 'react-native-maps';
-// import MapViewDirections from 'react-native-maps-directions';
+import MapView, { Callout, Circle, Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import { RootMyPropertiesStackNavigation } from '../navigation/MyPropertiesStackNavigation';
 import { useLocation } from '../hooks/useLocation';
 import Fab from '../components/ui/Fab';
+import Loading from '../components/ui/Loading';
 
 interface Props
   extends StackScreenProps<RootMyPropertiesStackNavigation, 'MapScreen'> {}
 
 const MapScreen = ({ route, navigation }: Props) => {
-  const [iconsVisible, setIconsVisible] = useState(false);
+  const [iconsVisible, setIconsVisible] = useState<Boolean>(false);
+  const [routeVisible, setRouteVisible] = useState<Boolean>(false);
   const { latitude, longitude, isExactLocation } = route.params;
   const {
     loading,
@@ -22,12 +24,15 @@ const MapScreen = ({ route, navigation }: Props) => {
   } = useLocation();
   const mapViewRef = useRef<MapView>();
   const following = useRef<boolean>(true);
-  // const destination = { latitude, longitude };
-  // const origin = {
-  //   latitude: userLocation.latitude,
-  //   longitude: userLocation.longitude,
-  // };
+  const destination = { latitude, longitude };
+
+  const origin = {
+    latitude: userLocation.latitude,
+    longitude: userLocation.longitude,
+  };
+
   const toggleMenu = () => setIconsVisible(!iconsVisible);
+  const createRoute = () => setRouteVisible(!routeVisible);
 
   const centerPosition = async () => {
     const { latitude, longitude } = await getCurrentLocation();
@@ -71,7 +76,7 @@ const MapScreen = ({ route, navigation }: Props) => {
   return (
     <View style={{ flex: 1 }}>
       {!loading ? (
-        <Text>Estamos ubicándote en el mapa</Text>
+        <Loading />
       ) : (
         <MapView
           onTouchStart={() => {
@@ -90,13 +95,24 @@ const MapScreen = ({ route, navigation }: Props) => {
             latitudeDelta: 0.0142,
             longitudeDelta: 0.0151,
           }}>
-          {/* <MapViewDirections
-            origin={origin}
-            destination={destination}
-            apikey={'AIzaSyAd22YBCutdzEZePBY2wbS2OawTZ1_H7-s'}
-          /> */}
+          {routeVisible ? (
+            <MapViewDirections
+              origin={origin}
+              destination={destination}
+              apikey={'AIzaSyAd22YBCutdzEZePBY2wbS2OawTZ1_H7-s'}
+              strokeWidth={4}
+              strokeColor="#3F19F9"
+            />
+          ) : (
+            <></>
+          )}
+
           {isExactLocation ? (
-            <Marker coordinate={{ latitude, longitude }} />
+            <Marker coordinate={{ latitude, longitude }}>
+              <Callout tooltip>
+                <Text style={styles.callout}>$ 123,123 MXN</Text>
+              </Callout>
+            </Marker>
           ) : (
             <Circle
               center={{ latitude, longitude }}
@@ -109,23 +125,21 @@ const MapScreen = ({ route, navigation }: Props) => {
 
       {iconsVisible && (
         <>
-          {/* <Fab
+          <Fab
             iconName="navigate-circle-outline"
-            onPress={() => {
-              console.log('crear ruta');
-            }}
-            style={{ position: 'absolute', bottom: 190, right: 10 }}
+            onPress={createRoute}
+            style={{ position: 'absolute', bottom: 190, left: 10 }}
             iconStyle={{ left: 1 }}
-          /> */}
+          />
           <Fab
             iconName="location-outline"
             onPress={propertyPosition}
-            style={{ position: 'absolute', bottom: 130, right: 10 }}
+            style={{ position: 'absolute', bottom: 130, left: 10 }}
           />
           <Fab
             iconName="compass-outline"
             onPress={centerPosition}
-            style={{ position: 'absolute', bottom: 70, right: 10 }}
+            style={{ position: 'absolute', bottom: 70, left: 10 }}
             iconStyle={{ left: 1 }}
           />
         </>
@@ -133,10 +147,21 @@ const MapScreen = ({ route, navigation }: Props) => {
       <Fab
         iconName={!iconsVisible ? 'chevron-up-outline' : 'chevron-down-outline'}
         onPress={toggleMenu}
-        style={{ position: 'absolute', bottom: 10, right: 10 }}
+        style={{ position: 'absolute', bottom: 10, left: 10 }}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  callout: {
+    color: 'white',
+    fontSize: 18,
+    backgroundColor: '#3F19F9',
+    borderRadius: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+});
 
 export default MapScreen;
