db922c60593325e3c6781cd1b66cc22abd567b45
JSegovia <acosta.segoviaa@gmamil.com>
2021-11-11 16:18:47 -0500

    Se crea useProperties

---
+++ src/hooks/useProperties.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import firestore from '@react-native-firebase/firestore';
+
+interface DocumentData {
+  [key: string]: any;
+}
+
+export const useProperties = (propertyType: string, action: string) => {
+  const [loading, setLoading] = useState<Boolean>(true);
+  const [properties, setProperties] = useState<DocumentData>([]);
+
+  const getProperties = () => {
+    try {
+      const data = firestore()
+        .collection('production')
+        .doc('witideal')
+        .collection('genders')
+        .doc(propertyType)
+        .collection(action);
+
+      const list: DocumentData = [];
+
+      return data.onSnapshot(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          if (doc.exists) {
+            const data = doc.data();
+            list.push({
+              id: doc.id,
+              data,
+            });
+          }
+        });
+        if (loading) {
+          setLoading(false);
+        }
+        setProperties(list);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProperties();
+  }, []);
+
+  return { loading, properties };
+};
