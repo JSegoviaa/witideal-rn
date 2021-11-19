e6de08e052f8c4b70e59627faba212d86de877b8
JSegovia <acosta.segoviaa@gmamil.com>
2021-11-19 18:03:32 -0500

    Se abre galería de cámara para seleccionar fotos del inmueble

--- src/components/AddPropertyScreen/UploadPropertyPictures.tsx
+++ src/components/AddPropertyScreen/UploadPropertyPictures.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { launchImageLibrary } from 'react-native-image-picker';
 
 const UploadPropertyPictures = () => {
+  const [tempUri, setTempUri] = useState<string>('');
+  const [fileName, setFileName] = useState<string>('');
+
+  const takePhotoFromGallery = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        quality: 0.5,
+      },
+      resp => {
+        if (resp.didCancel) return;
+        if (!resp.assets![0].uri) return;
+        setTempUri(resp.assets![0].uri);
+        setFileName(resp.assets![0].fileName!);
+      },
+    );
+  };
+
   return (
     <View>
       <View>
-        <Text>Foto principal</Text>
-        <TouchableOpacity>
-          <Text>Selecciona la imagen principal de tu inmueble</Text>
+        <Text style={styles.subtitle}>Foto principal</Text>
+        <TouchableOpacity
+          style={styles.background}
+          onPress={takePhotoFromGallery}>
+          <Text style={styles.subtitle}>
+            Selecciona la imagen principal de tu inmueble
+          </Text>
+          <Text style={styles.text}>Solo se subir una foto</Text>
         </TouchableOpacity>
       </View>
       <View>
-        <Text>Fotos del inmueble</Text>
-        <TouchableOpacity>
-          <Text>Selcciona las fotos que quieres mostrar en tu inmueble</Text>
+        <Text style={styles.subtitle}>Fotos del inmueble</Text>
+        <TouchableOpacity
+          style={styles.background}
+          onPress={takePhotoFromGallery}>
+          <Text style={styles.subtitle}>
+            Selcciona las fotos que quieres mostrar en tu inmueble
+          </Text>
+          <Text style={styles.text}>Máximo 30 fotos</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -22,4 +51,23 @@ const UploadPropertyPictures = () => {
 
 export default UploadPropertyPictures;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#E0D7EE',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderStyle: 'dotted',
+  },
+  subtitle: {
+    color: '#1E0E6F',
+    fontSize: 18,
+    fontWeight: '700',
+    paddingVertical: 10,
+    textAlign: 'center',
+  },
+  text: {
+    color: '#000',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
