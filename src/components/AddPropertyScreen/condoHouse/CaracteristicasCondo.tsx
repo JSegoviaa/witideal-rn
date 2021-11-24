3df9d18473e1844b6dd86f6ce8dc394062a6caa8
JSegovia <acosta.segoviaa@gmamil.com>
2021-11-24 13:35:32 -0500

    orden de caracteristics fisicas condohouse

--- src/components/AddPropertyScreen/condoHouse/CaracteristicasCondo.tsx
+++ src/components/AddPropertyScreen/condoHouse/CaracteristicasCondo.tsx
@@ -9,20 +9,6 @@ const CaracteristicasCondo = () => {
     <View>
       <TextInput
         keyboardType="numeric"
-        onChangeText={value => onChange(value, 'm2Build')}
-        style={appStyles.input}
-        placeholder="M2 construidos"
-        placeholderTextColor="#ccc"
-      />
-      <TextInput
-        keyboardType="numeric"
-        onChangeText={value => onChange(value, 'totalUnits')}
-        style={appStyles.input}
-        placeholder="Total de unidades del conjunto"
-        placeholderTextColor="#ccc"
-      />
-      <TextInput
-        keyboardType="numeric"
         onChangeText={value => onChange(value, 'm2Terrain')}
         style={appStyles.input}
         placeholder="M2 del terreno"
@@ -30,9 +16,23 @@ const CaracteristicasCondo = () => {
       />
       <TextInput
         keyboardType="numeric"
+        onChangeText={value => onChange(value, 'm2Build')}
+        style={appStyles.input}
+        placeholder="M2 construidos"
+        placeholderTextColor="#ccc"
+      />
+      <TextInput
+        keyboardType="numeric"
         onChangeText={value => onChange(value, 'floors')}
         style={appStyles.input}
         placeholder="Niveles construidos"
+        placeholderTextColor="#ccc"
+      />
+      <TextInput
+        keyboardType="numeric"
+        onChangeText={value => onChange(value, 'totalUnits')}
+        style={appStyles.input}
+        placeholder="Total de unidades del conjunto"
         placeholderTextColor="#ccc"
       />
     </View>
