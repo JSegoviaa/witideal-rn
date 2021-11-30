d621cad14e8589376eb1bbaf7923f5012f55ef71
JSegovia <acosta.segoviaa@gmamil.com>
2021-11-30 12:16:20 -0500

    Se agrega botón para compartir inmueble

--- src/components/PropertyDetailScreen/Images.tsx
+++ src/components/PropertyDetailScreen/Images.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
-import { FlatList, Image, useWindowDimensions, View } from 'react-native';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import Share from 'react-native-share';
+import Icon from 'react-native-vector-icons/Ionicons';
+import urlTranslator from '../../helpers/urlTranslator';
 import { useProperty } from '../../hooks/useProperty';
+import Fab from '../ui/Fab';
 
 interface Props {
   id: string;
@@ -11,6 +22,20 @@ interface Props {
 const Images = ({ id, action, propertyType }: Props) => {
   const { width } = useWindowDimensions();
   const { property } = useProperty(id, action, propertyType);
+
+  const handleShare = () => {
+    Share.open({
+      url: `https://witideal.com/propiedad/${urlTranslator(
+        propertyType,
+      )}/${urlTranslator(action)}/${id}`,
+    })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        err && console.log(err);
+      });
+  };
 
   return (
     <View>
@@ -31,8 +56,24 @@ const Images = ({ id, action, propertyType }: Props) => {
           horizontal={true}
         />
       )}
+
+      <Fab
+        iconName="share-social-outline"
+        onPress={handleShare}
+        style={styles.share}
+        iconStyle={{ right: 1 }}
+      />
     </View>
   );
 };
 
 export default Images;
+
+const styles = StyleSheet.create({
+  share: {
+    bottom: 10,
+    left: 10,
+    padding: 4,
+    position: 'absolute',
+  },
+});
